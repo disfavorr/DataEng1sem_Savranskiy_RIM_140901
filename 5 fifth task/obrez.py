c56045a9abf4c88d3ef059f8c636b36fab6f913f
disfavorr <savranskiy.egor@mail.ru>
2024-12-03 22:41:04 +0500

    Update obrez.py

--- 5 fifth task/obrez.py
+++ 5 fifth task/obrez.py
@@ -3,6 +3,6 @@ import pandas as pd
 file_path = "cds.csv"  
 data = pd.read_csv(file_path)
 
-new_date = data[:500000]
+new_date = data[:100000]
 
-selected_data.to_csv("selected_data.csv", index=False)
+new_date.to_csv("data1.csv", index=False)
